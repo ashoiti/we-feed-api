6bbfec70e83a8891fd9c72f7c19b71f2525b5b9f
Edu Bernardo <eduluizbernardo@gmail.com>
2022-10-29 10:53:33 -0300

    fix: enabling cors to auth controller

--- src/main/java/br/com/wefeed/controller/AuthController.java
+++ src/main/java/br/com/wefeed/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import br.com.wefeed.controller.dto.UserDTO;
 import br.com.wefeed.model.User;
 import br.com.wefeed.service.UserService;
 
+@CrossOrigin
 @RestController
 public class AuthController {
 	
